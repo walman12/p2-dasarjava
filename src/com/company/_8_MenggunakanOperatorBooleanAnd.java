ffd974d415e0e6c5819ab4b63586e991318e7afe
HP <kanwalman25@gmail.com>
2022-03-28 16:20:14 +0700

    percobaan java

--- src/com/company/_8_MenggunakanOperatorBooleanAnd.java
+++ src/com/company/_8_MenggunakanOperatorBooleanAnd.java
@@ -1,6 +1,6 @@
 package com.company;
 
-public class _8_MenggunakanOperatorBooleanAnd {
+public class  _8_MenggunakanOperatorBooleanAnd {
     public static void main(String[] args) {
         int a = 5, b = 7;
         if ((a<2) & (b++< 10)){
